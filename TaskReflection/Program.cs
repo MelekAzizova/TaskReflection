835047e8768055ea16ad3b58fcf7bca7cdc0e6eb
MelekAzizova <147470006+MelekAzizova@users.noreply.github.com>
2023-11-03 01:33:57 +0400

    Add project files.

---
+++ TaskReflection/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace TaskReflection
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            User user = new User();
+            var prop = user.GetType().GetField("age", System.Reflection.BindingFlags.NonPublic
+    | System.Reflection.BindingFlags.Instance);
+            prop.SetValue(user,21);
+            Console.WriteLine(prop.GetValue(user));
+
+        }
+    }
+}
---
+++ TaskReflection/User.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskReflection
+{
+    internal class User
+    {
+        private int id;
+
+        private string name;
+        private static int age = 26;
+
+
+        public int Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public int Age
+        {
+            get { return age; }
+            set { age = value; }
+        }
+
+
+
+        static void ChangeAge(int age)
+        {
+            age = age;
+
+        }
+
+
+    }
+}
