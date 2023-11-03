74ef5a4cec994644b38e06ca58dbfef52ca77f9c
MelekAzizova <147470006+MelekAzizova@users.noreply.github.com>
2023-11-03 12:37:46 +0300

    Update Program.cs

--- TaskReflection/Program.cs
+++ TaskReflection/Program.cs
@@ -7,7 +7,11 @@
             User user = new User();
             var prop = user.GetType().GetField("age", BindingFlags.NonPublic
     | BindingFlags.Static);
-            prop.SetValue(user,21);
+             prop.SetValue(user,21);
+            Console.WriteLine(prop.GetValue(user));
+            var prop2=user.GetType().GetField("name", BindingFlags.NonPublic
+    | BindingFlags.Instance);
+            prop.SetValue(user,"Melek");
             Console.WriteLine(prop.GetValue(user));
 
         }
